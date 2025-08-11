e1f367d7ffd960501bf104db92d4ba947b4a721c
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-08-12 02:07:58 +0900

    Remove unused CSS files and refactor layout and page components to integrate socket.io for real-time connection status

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,4 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-
 import type { Metadata } from 'next';
-import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -25,9 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
