dd153346d5db03fd6afb4bac333f888789455b2c
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-08-17 23:27:24 +0900

    Integrate AppRouterCacheProvider into RootLayout for improved routing support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -12,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
