9e60c956ca8e199b89f51d98b78ccee5d7e29c72
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-08-18 19:19:42 +0900

    Refactor socket.ts to add type definitions for socket connections

--- src/socket.ts
+++ src/socket.ts
@@ -1,11 +1,22 @@
 'use client';
 
-import { io } from 'socket.io-client';
+import { io, type Socket } from 'socket.io-client';
+
+import type {
+  AdminClientToServerEvents,
+  AdminServerToClientEvents,
+  ClientToServerEvents,
+  ServerToClientEvents,
+} from '../server/src/sockets/events';
 
 const URL =
   process.env.NODE_ENV === 'production'
     ? process.env.SOCKET_SERVER_URL
     : 'http://localhost:4000';
 
-export const socket = io(URL);
-export const adminSocket = io(URL + '/admin');
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+  io(URL);
+export const adminSocket: Socket<
+  AdminServerToClientEvents,
+  AdminClientToServerEvents
+> = io(URL + '/admin');
