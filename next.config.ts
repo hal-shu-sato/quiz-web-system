e397e1230c87d808dca99d828c9c15b9cb9a7782
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-08-31 02:23:02 +0900

    Update next.config.ts for Konva compatibility and modify dev script in package.json

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    esmExternals: 'loose', // required to make Konva & react-konva work
+  },
+  webpack: (config: { externals: Record<string, string>[] }) => {
+    config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+    return config;
+  },
 };
 
 export default nextConfig;
