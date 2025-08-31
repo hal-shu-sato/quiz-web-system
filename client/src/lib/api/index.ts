6dbeb63b69369e227e1574e28b6aeee1b7350eb6
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-09-01 01:32:01 +0900

    Add API client setup using openapi-fetch and openapi-react-query

---
+++ client/src/lib/api/index.ts
@@ -0,0 +1,11 @@
+import createFetchClient from 'openapi-fetch';
+import createClient from 'openapi-react-query';
+
+import type { paths } from './openapi';
+
+const fetchClient = createFetchClient<paths>({
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:4000/',
+});
+const $api = createClient(fetchClient);
+
+export default $api;
