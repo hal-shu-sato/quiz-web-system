a422352256f92ca508c0cf52ca7399ce8e475d52
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-09-01 00:39:58 +0900

    Refactor ESLint configuration to include recommended import plugin settings and simplify react pattern in import order

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import pluginJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -26,6 +27,11 @@ export default tseslint.config(
     },
   },
   {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
     rules: {
       'import/order': [
         'error',
@@ -42,7 +48,7 @@ export default tseslint.config(
 
           pathGroups: [
             {
-              pattern: '{react,react-dom/**,react-router-dom}',
+              pattern: '{react,react-dom/**}',
               group: 'builtin',
               position: 'before',
             },
