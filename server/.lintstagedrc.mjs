b32f8ac15cb214383eb359652d3a07c031cbcd33
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-08-31 23:22:23 +0900

    Refactor lint-staged configuration to replace dynamic ESLint command generation with a static command

--- server/.lintstagedrc.mjs
+++ server/.lintstagedrc.mjs
@@ -1,20 +1,14 @@
-import path from 'node:path';
-
-/** @type {import('lint-staged').SyncGenerateTask} */
-const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
-    .map((f) => '"' + path.relative(process.cwd(), f) + '"')
-    .join(' ')}`;
+const eslintCommand = 'eslint --fix';
 const prettierCommand = 'prettier --write';
 
 /** @type {import('lint-staged').Configuration} */
 const lintStagedConfig = {
   '*.{ts,tsx}': [
     () => 'tsc --incremental false --noEmit',
-    buildEslintCommand,
+    eslintCommand,
     prettierCommand,
   ],
-  '*.{js,jsx}': [buildEslintCommand, prettierCommand],
+  '*.{js,jsx}': [eslintCommand, prettierCommand],
   '*.{json,yml,md,html,css,scss}': prettierCommand,
 };
 
