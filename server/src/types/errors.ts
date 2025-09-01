a1fbe23822f1315245354f32a342ccb45fd43c23
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-09-01 15:21:47 +0900

    Reorganize error interfaces in errors.ts for improved clarity and structure

--- server/src/types/errors.ts
+++ server/src/types/errors.ts
@@ -1,9 +1,23 @@
-export interface ValidateErrorJSON {
-  message: 'Validation failed';
+// 401 Unauthorized
+export interface UnauthorizedErrorJSON {
+  message: 'Unauthorized';
   details: { [name: string]: unknown };
 }
 
+// 403 Forbidden
+export interface ForbiddenErrorJson {
+  message: 'Forbidden';
+  details: { [name: string]: unknown };
+}
+
+// 404 Not Found
 export interface NotFoundErrorJSON {
   message: 'Not found';
+  details: { [name: string]: unknown };
+}
+
+// 422 Unprocessable Content
+export interface ValidateErrorJSON {
+  message: 'Validation failed';
   details: { [name: string]: unknown };
 }
