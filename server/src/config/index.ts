035d34eb2f2b88fdb4236c81ae593ff4e1750ff4
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-09-02 01:58:04 +0900

    Add redisUrl configuration to support Redis connection

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -2,6 +2,7 @@ const config = {
   port: process.env.PORT || 4000,
   corsOrigin: process.env.SERVER_CORS_ORIGIN || 'http://localhost:3000',
   sessionSecret: process.env.SESSION_SECRET || 's3Cur3',
+  redisUrl: process.env.REDIS_URL || 'redis://:redispass@localhost:6379',
 };
 
 export default config;
