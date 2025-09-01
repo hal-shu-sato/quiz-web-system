039d674fb8e1cc38db6a64463dbe2a9c71948181
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2025-09-01 17:03:36 +0900

    Refactor error handling classes to improve structure and consistency

--- server/src/types/errors.ts
+++ server/src/types/errors.ts
@@ -1,7 +1,22 @@
+import type { Exception, FieldErrors } from 'tsoa';
+
 // 401 Unauthorized
 export interface UnauthorizedErrorJSON {
   message: 'Unauthorized';
   details: { [name: string]: unknown };
+}
+
+export class UnauthorizedError extends Error implements Exception {
+  public status = 401;
+  public name = 'UnauthorizedError';
+
+  constructor(
+    public fields: FieldErrors,
+    public message: string,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, UnauthorizedError.prototype);
+  }
 }
 
 // 403 Forbidden
@@ -10,10 +25,36 @@ export interface ForbiddenErrorJson {
   details: { [name: string]: unknown };
 }
 
+export class ForbiddenError extends Error implements Exception {
+  public status = 403;
+  public name = 'ForbiddenError';
+
+  constructor(
+    public fields: FieldErrors,
+    public message: string,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, ForbiddenError.prototype);
+  }
+}
+
 // 404 Not Found
 export interface NotFoundErrorJSON {
   message: 'Not found';
   details: { [name: string]: unknown };
+}
+
+export class NotFoundError extends Error implements Exception {
+  public status = 404;
+  public name = 'NotFoundError';
+
+  constructor(
+    public fields: FieldErrors,
+    public message: string,
+  ) {
+    super(message);
+    Object.setPrototypeOf(this, NotFoundError.prototype);
+  }
 }
 
 // 422 Unprocessable Content
